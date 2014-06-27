0b0e1fbaa0a1a82d092e3329fd98c091f8d00d04
jryu <jryu@users.noreply.github.com>
2014-06-27 22:00:11 +0000

    Place SECRET_KEY in secret_key.py
    Created secret_key.py which contains a single variable 'key' which
    stores SECRET_KEY value created by django.

--- sgtd/sgtd/settings.py
+++ sgtd/sgtd/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import secret_key
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -17,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = secret_key.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
