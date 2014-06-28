9f6f0c344a2c6ec0551a44851840832dd6496d8a
jryu <jryu@users.noreply.github.com>
2014-06-28 13:56:02 +0000

    Create stuff
    Using CreateView instead of ListView because it's simpler to add a list
    of stuff in CreateView rather than adding form and POST handler to the
    ListView.

--- sgtd/things/views.py
+++ sgtd/things/views.py
@@ -1,9 +1,16 @@
+from django.core.urlresolvers import reverse
 from django.views import generic
 
 from things.models import Thing
 
-class StuffListView(generic.ListView):
+class StuffListView(generic.CreateView):
     template_name = 'stuff_list.html'
+    model = Thing
 
-    def get_queryset(self):
-        return Thing.objects.order_by('-datetime_update')
+    def get_context_data(self, **kwargs):
+        context = super(StuffListView, self).get_context_data(**kwargs)
+        context['object_list'] =  Thing.objects.order_by('-datetime_create')
+        return context
+
+    def get_success_url(self):
+        return reverse('stuff_list')
