481238ee6b9fa21fdb43ce97a5c5b6998babdf91
J <jryu@users.noreply.github.com>
2015-12-05 08:09:42 +0000

    Uncheck todo item when checked one is clicked

--- sgtd/todo/urls.py
+++ sgtd/todo/urls.py
@@ -4,5 +4,6 @@ from todo import views
 
 urlpatterns = patterns('',
     url(r'^$', views.TodoListView.as_view(), name='todo_list'),
-    url(r'^log/$', views.LogCreateView.as_view(), name='todo_log')
+    url(r'^check/$', views.LogCreateView.as_view(), name='todo_check'),
+    url(r'^uncheck/$', views.LogDeleteView.as_view(), name='todo_uncheck'),
 )
