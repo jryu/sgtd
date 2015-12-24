c746ec29d0e53e02adcb110a217e6caad77c1a2e
Junho Ryu <jryu@users.noreply.github.com>
2015-12-24 15:56:58 -0800

    Update manage.py commands for Django 1.9

--- sgtd/dumpdata.sh
+++ sgtd/dumpdata.sh
@@ -1,10 +1,13 @@
+set -e
+
 TIMESTAMP=`date +%s`
+
+./manage.py dumpdata --indent=2 \
+	things todo auth.User sites sessions socialaccount | \
+	tee ~/dump.$TIMESTAMP.json
 
 mv db.sqlite3 ~/db.$TIMESTAMP.sqlite3
 
-./manage.py dumpdata --indent=2 things todo auth.User | \
-	tee ~/dump.$TIMESTAMP.json
-
-./manage.py syncdb --noinput --no-initial-data
+./manage.py migrate --noinput
 
 ./manage.py loaddata ~/dump.$TIMESTAMP.json
